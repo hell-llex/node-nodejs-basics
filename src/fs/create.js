54761a063ff3b984c2e35b26a62b633ac556f4f0
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-07 02:27:05 +0300

    feat: add fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { stat, writeFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const content = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fresh.txt');
+
+    try {
+        await stat(filePath);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await writeFile(filePath, content);
+            console.log('--------- File created successfully ---------\n');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
