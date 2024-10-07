58db284588e367fb6e9c4f9562d803b746a5c499
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-07 23:54:38 +0300

    feat: add stream/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToRead.txt');
+    try {
+        const stream = createReadStream(filePath, 'utf-8');
+        stream.pipe(process.stdout);
+        stream.on('end', () => console.log('\n'));
+        stream.on('error', (err) => console.error('Error while reading the file:', err));
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
 };
 
 await read();
