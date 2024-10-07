28b1af31b6c4c7ce55e4a199b9d2cc5f142df031
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-07 04:01:42 +0300

    feat: add fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import { stat, rename as renameFs } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+    const oldFileName = join(__dirname, 'files', 'wrongFilename.txt');
+    const newFileName = join(__dirname, 'files', 'properFilename.md');
+
+    try {
+        await stat(oldFileName);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await stat(newFileName);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await renameFs(oldFileName, newFileName);
+            console.log('--------- File renamed successfully ---------\n');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await rename();
