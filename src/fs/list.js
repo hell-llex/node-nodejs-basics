82e55715c46485bbf647e3b479adb15eaf54fef8
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-07 03:38:59 +0300

    feat: add fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import { readdir, stat } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+    const dirPath = join(__dirname, 'files');
+
+    try {
+        await stat(dirPath);
+        const files = await readdir(dirPath);
+        console.log(`----- Files in the directory: -----\n`);
+        console.log(files);
+        console.log('\n---------------------------------\n');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await list();
