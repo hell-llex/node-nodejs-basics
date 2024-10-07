8ab0379e22f4c2841a50f3f338c546c2e10825f7
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-07 23:29:37 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+    stream.on('data', (chunk) => hash.update(chunk));
+    stream.on('end', () => {
+        const fileHash = hash.digest('hex');
+        console.log(`Hash: ${fileHash} \n`);
+    });
+    stream.on('error', (err) => console.error('Error while reading the file:', err));
 };
 
 await calculateHash();
