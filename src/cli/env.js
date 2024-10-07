6c2dfb9c0d6fff712d122fc85acef5e8b53bf270
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-07 04:35:01 +0300

    feat: add cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+// при использовании на macOS нужно запускать с использованием команды sudo 
+// sudo npm run cli:env
+// также установить cross-env если не установленно на устройстве
 const parseEnv = () => {
-    // Write your code here 
+    const envVariables = process.env;
+    const rssVariables = [];
+    for (const [key, value] of Object.entries(envVariables)) {
+        if (key.startsWith('RSS_')) {
+            rssVariables.push(`${key}=${value}`);
+        }
+    }
+    console.log(rssVariables.join('; '), '\n');
 };
 
 parseEnv();
