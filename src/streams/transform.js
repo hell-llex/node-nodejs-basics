6d29b140fd89769706bd14530a57bb193b85c503
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-08 00:38:04 +0300

    feat: add stream/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    console.log('Enter text and press Ctrl + D (or Ctrl + Z on Windows) to finish.\n');
+    const reverseStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversed = chunk.toString().split('').reverse().join('') + '\n\n';
+            callback(null, reversed);
+        }
+    });
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
