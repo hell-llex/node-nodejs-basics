4b48d37379b2f381426bbdc1e89021d47cd29522
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-08 00:14:39 +0300

    feat: add stream/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+    try {
+        console.log('Enter text and press Ctrl + D (or Ctrl + Z on Windows) to finish.\n');
+        const writeStream = createWriteStream(filePath, { flags: 'w' });
+        process.stdin.pipe(writeStream);
+        writeStream.on('finish', () => console.log('\nWriting completed.\n'));
+        writeStream.on('error', (error) => console.error('Error while writing to file:', error.message));
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
 };
 
 await write();
