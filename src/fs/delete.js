67a90f534204cd026d4ae21c5491f8aff7fec20c
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-07 03:22:50 +0300

    feat: add fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import { unlink, stat } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+    const fileToRemove = join(__dirname, 'files', 'fileToRemove.txt'); 
+
+    try {
+        await stat(fileToRemove);
+        await unlink(fileToRemove);
+        console.log('--------- File removed successfully ---------\n');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await remove();
