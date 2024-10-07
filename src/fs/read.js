47dc12fbbacf8b2cb50aa4a6f6a7ee9c3058761c
Alexander Demeshchenko <demeshenko333@gmail.com>
2024-10-07 03:47:10 +0300

    feat: add fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+import { stat, readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToRead.txt'); 
+
+    try {
+        await stat(pathToFile);
+        const data = await readFile(pathToFile, 'utf-8');
+        console.log('--------- File info: ---------\n');
+        console.log(data);
+        console.log('\n----------------------------\n');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await read();
